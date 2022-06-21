f00be683e3d9f902befa30d91f5dd3c2c012bc4f
andres ramos <andresramos24@hotmail.com>
2022-06-21 17:55:03 -0500

    Terminado todo el index.php

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>  Curso   udemy </title>
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet"> 
+    <link rel="preload" href="css/estilos.css"  as="style">
+    <link rel="stylesheet" href="css/estilos.css">
+    
+
+</head>
+<body>
+
+<section>
+    <!-- <img src="img/login.png" alt=""></section> -->
+
+
+<header> 
+
+  
+<h1 class="titulo">Andres  Ramos  <span> Ingeniero Informatico  </span> </h1>
+
+<div class="nav-bg">
+    <nav class="navegacion-principal  contenedor">
+        <a href="#">  Inicio</a>
+        <a href="#">  Sobre  Mi </a>
+        <a href="#">  Clientes  </a>
+        <a href="#">  Contacto </a>   
+
+    </nav>
+
+</div>
+<section  class="hero">
+    <div class="contenido-hero">
+                <h2  class="titulo"> Diseño y Desarrollo  Web </h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" 
+                    width="96" height="96" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffec00" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="12" cy="11" r="3" />
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                    </svg>
+                    
+                <p> Monteria  Cordoba </p>
+                <a  class="boton"   href="#"> Contactar</a>
+
+    </div>
+</section>
+
+<main  class="contenedor  sombra">
+    <center><h2>  Mis Servicios</h2></center> 
+
+    <div class="servicios">
+            <section class="servicio">
+                <h3> Diseño y Desarrollo de Paginas  Web</h3>
+                <div class="iconos">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler 
+                        icon-tabler-brand-html5" width="96" height="96" viewBox="0 0 24 24" 
+                        stroke-width="1.5" stroke="#ffec00" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
+                        <path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
+                        </svg>
+                </div>    
+                <p> Diseño y desarrollo de paginas  web dinamicas, adaptables y seguras, servicio  de administracion web, diseño de  imagenes  y logos </p>
+            </section>
+
+            <section class="servicio">
+                <h3> Desarrollo  de Programas  </h3>
+                <div class="iconos">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler 
+                        icon-tabler-settings-automation" width="96" height="96" viewBox="0 0 24 24" 
+                        stroke-width="1.5" stroke="#ffec00" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+                        <path d="M10 9v6l5 -3z" />
+                        </svg>
+                </div>        
+                <p> Desarrollo de programas a la medida, programas de escritorio, programas  contables, de  prestamos entre otros  </p>
+            </section>
+
+            <section class="servicio">
+                <h3> Soporte y Mantenimiento de Pc</h3>
+                <div class="iconos">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-devices-pc"
+                        width="96" height="96" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffec00" fill="none" 
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M3 5h6v14h-6z" />
+                        <path d="M12 9h10v7h-10z" />
+                        <path d="M14 19h6" />
+                        <path d="M17 16v3" />
+                        <path d="M6 13v.01" />
+                        <path d="M6 16v.01" />
+                        </svg>
+                </div>        
+                <p> Reparacion de  computadores, formateo instalacion de  programas </p>
+            </section>
+
+    </div>
+
+    <section>
+        <h2>  Contacto </h2>
+        <form  class="formulario" action="">
+        <legend> Contactanos llenando todos los datos </legend>
+
+            <fieldset>
+
+            <div class="contenedor-campos">
+                    <div class="campo">
+                        <label for=""> Nombres  </label>
+                        <input class="input-text" type="text"  placeholder="Tu nombre" name="" id="">
+
+                    </div>
+                                      
+                    <div class="campo">
+                        <label for=""> Telefono   </label>
+                        <input class="input-text" type="tel"   placeholder=" Tu telefono">
+                    </div>
+                    <div class="campo">
+                        <label for=""> Correo Electronico </label>
+                        <input class="input-text" type="email" placeholder="Tu Email">
+                    </div>
+                    
+                    <div class="campo">
+                        <label for="">Mensaje</label>
+                        <textarea  class="input-text"></textarea>
+                    </div>
+
+            </div>  
+            
+
+                <div  class="enviar">
+                    <input   class="boton"  type="submit" value="Enviar">
+                </div>
+                
+            </fieldset>
+
+        </form>
+    </section>
+</main>      
+
+
+<footer  class="footer">
+     <p> Todos  los  derechos  reservados  </p>
+</footer>
+    
+</body>
+</html>
